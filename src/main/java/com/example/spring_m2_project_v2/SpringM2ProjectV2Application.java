2814a09731441f3a7311a73bd4f0cc719de52c2a
Fake4091 <aswends7517@gmail.com>
2025-02-27 10:07:32 -0600

    Initial Commit

---
+++ src/main/java/com/example/spring_m2_project_v2/SpringM2ProjectV2Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_m2_project_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringM2ProjectV2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringM2ProjectV2Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/spring_m2_project_v2/SpringM2ProjectV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_m2_project_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringM2ProjectV2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
